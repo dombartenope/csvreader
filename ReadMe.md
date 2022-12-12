41eaca1df1c7f2d16993cc341cc03ccdcb4a5d1e
dominick bartenope <dominickbartenope@dominicks-MacBook-Pro-2.local>
2022-12-12 14:11:55 -0500

    Updated ReadMe. Added search functionality in terminal, and bool that handles colored output

--- ReadMe.md
+++ ReadMe.md
@@ -2,8 +2,11 @@
 
 1. Save this code in a file named file.cpp
 2. Make sure that the input file, file.csv, is in the same directory as file.cpp
-3. Compile the code using a C++ compiler, such as g++: g++ file.cpp -o file
-4. Add your query to the search condition
+3. Compile the code using a C++ compiler, such as g++: 
+`g++ file.cpp -o file`
+4. Add your query when prompted in the terminal to search
 5. Run the compiled program: ./file
 
 ## The program will read the input file, file.csv, and create a new output file, output.csv, in the same directory. The output file will contain the same data as the input file, but only the lines that contain the string will be written to the output file.
+
+### Change the color of your terminal output by setting colorMode to true. (This only applies if your terminal accepts ANSI Color Codes)
--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 
 int main() {
+  double count {0};
 
   // Open the input and output files
   std::ifstream input("file.csv");
@@ -16,16 +17,29 @@ int main() {
   // Write the "column name" line to the output file
   output << line << "\n";
 
+  //Switch to true if your terminal supports ANSI Color Codes
+  bool colorMode {true};
+  
+  
+  //Request for the user to enter in a search term to look for in the csv file
+  std::string search;
+  std::cout << "\nEnter a string that you would like to find exact matches for: ";
+  std::cin >> search;
+
+
+
   // Loop through the rest of the lines in the input file
   while (std::getline(input, line))
   {
-      // SEARCH CONDITION:
-      if (line.find("dom") != std::string::npos)
+      // SEARCH CONDITION (this currently does not support spaces and will only search for the first string input):
+      if (line.find(search) != std::string::npos)
       {
+          count++;
           // Write the line to the output file
           output << line << "\n";
       }
   }
+  std::cout << (colorMode ? "\033[\n1;31mFound " : "\nFound") << count << " matches. Please see output.csv to view your updated CSV.\n"<< std::endl;
 
   // Close the files
   input.close();
